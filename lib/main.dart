410536578950038ce40d4675ca30e744c2fa1167
Josh <201913147g@utea.edu.pe>
2022-10-03 13:14:48 -0500

    ultimos mods

--- lib/main.dart
+++ lib/main.dart
@@ -16,25 +16,79 @@ class MyApp extends StatelessWidget {
             CircleAvatar(
               radius: 60.0,
               backgroundColor: Colors.white,
-              backgroundImage: NetworkImage(
-                  "https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg"),
+              backgroundImage: AssetImage("assets/images/persona2.jpeg"),
+            ),
+            SizedBox(
+              height: 10.0,
             ),
             Text(
-              "Fiorella de Fátima guadalupe",
+              "Fiorella de Fátima Guadalupe",
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 25.0,
                 fontWeight: FontWeight.bold,
+                fontFamily: "Oleo",
               ),
             ),
             Text(
               "FLUTTER DEVELOPER",
               style: TextStyle(
-                  color: Colors.white38,
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.w300,
-                  letterSpacing: 4.0),
-            )
+                color: Colors.white38,
+                fontSize: 18.0,
+                fontWeight: FontWeight.w300,
+                letterSpacing: 4.0,
+              ),
+            ),
+            Divider(
+              thickness: 1.0,
+              color: Colors.white38,
+              indent: 120.0,
+              endIndent: 120.0,
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+              elevation: 10.0,
+              child: ListTile(
+                title: Text("+51 928203145"),
+                subtitle: Text("Telefono"),
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.indigo,
+                ),
+                trailing: Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.indigo,
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+              elevation: 10.0,
+              child: ListTile(
+                title: Text("jorisuen5@gmail.com"),
+                subtitle: Text("Correo Electronico"),
+                leading: Icon(
+                  Icons.mail,
+                  color: Colors.indigo,
+                ),
+                trailing: Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.indigo,
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Icon(Icons.alarm),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Icon(Icons.alarm),
+                Icon(Icons.alarm),
+              ],
+            ),
           ],
         ),
       ),
